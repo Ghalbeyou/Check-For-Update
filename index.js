4f234c76f82e09171719449421a611deb4dfcb35
Ghalbeyou <97286379+Ghalbeyou@users.noreply.github.com>
2022-01-22 09:56:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// test
+// main
+const fs = require('fs');
+const cons = console();
+function main(){
+  if (ChkUpdate(1,0,0.5) == true){
+     cons.log("Checking update finished. Your version is up to date!");
+     app();
+  }else{
+     cons.error("you are not up to date ;(");
+     procces.quit();
+     }
+}
+function ChkUpdate(a){
+  var lastetver = "1.0.2";
+  if (a = lastetver){
+     return true;
+  } else {
+     return false;
+  }
+}
+function app()
+{
+   // your app here!
+}
+
+/*
+Replace app function to your app.
+replace lastetver varible to lastet version of your app.
+Warning! This app is a checker for updates.
+We are updating it. 
+Thanks!!
+*/
